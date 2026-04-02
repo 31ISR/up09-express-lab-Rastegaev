b7517a82ad555437eebcd504b5648d3fd4b9dbd5
maks <Maksimrastegaev742@gmail.com>
2026-04-02 16:34:59 +0000

    {ostalsya admin}

--- my-service/main.js
+++ my-service/main.js
@@ -62,7 +62,7 @@ app.post("/api/auth/login", (req, res) => {
 app.get("/api/auth/profile", auth, (req, res) => {
 
     try {
-        const user = db.prepare("SELECT * FROM User WHERE id = ?").get(req.user.id)
+        const user = db.prepare("SELECT * FROM User WHERE id = ?").get(res.id)
         if (!user) return res.status(401).json({ error: "ты накосячил гдето" })
         const { password: _, ...safeUser } = user
         return res.status(200).json({ success: true, safeUser, error: null })
@@ -72,12 +72,12 @@ app.get("/api/auth/profile", auth, (req, res) => {
         res.status(500).json({ error: "Somethin went wrong" })
     }
 })
-app.get("/api/books", auth, (req, res) => {
+app.get("/api/books", (req, res) => {
 
     try {
         const books = db.prepare("SELECT * FROM Book").all()
         if (!books) return res.status(401).json({ error: "ты накосячил гдето" })
-        
+
         return res.status(200).json({ success: true, books, error: null })
 
     } catch (error) {
@@ -85,11 +85,85 @@ app.get("/api/books", auth, (req, res) => {
         res.status(500).json({ error: "Somethin went wrong" })
     }
 })
-app.post("/api/books", auth, (req, res)=>{
+app.get("/api/books/:id", (req, res) => {
     try {
-        const book = db.prepare("INSERT INTO Books VALUES(?,?,?) ")
+        const book = db.prepare("SELECT * FROM Book WHERE id = ?").get(req.params.id)
+        res.status(200).json(book)
     } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Somethin went wrong" })
+    }
+})
+app.post("/api/books", auth, (req, res) => {
+    try {
+        const { title, author, year, genre, description } = req.body;
+        const userId = req.user.id;
+        const query = db.prepare(`INSERT INTO Book (title, author, year,genre,description,createdBy) VALUES (?, ?, ?, ?, ?, ?)`).run(title, author, year, genre, description, userId);
+        const newBook = db.prepare("SELECT * FROM Book WHERE id  = ?").get(query.lastInsertRowid)
+        res.status(201).json(newBook)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Somethin went wrong" })
+    }
+})
+app.delete("/api/books/:id", auth, (req, res) => {
+    try {
+        const bookId = req.params.id;
+        const deleteook = db.prepare("DELETE FROM Book WHERE id = ?").run(bookId);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Somethin went wrong" })
+    }
+})
+app.put("/api/books/:id", auth, (req, res) => {
+    try {
+        const { title, author, year, genre, description } = req.body;
+        const bookId = req.params.id;
+        const userId = req.user.id;
+        const query = db.prepare(`UPDATE Book
+      SET title = ?, author = ?, year = ?, genre = ?, description = ?, createdBy = ?
+      WHERE id = ?`).run(title, author, year, genre, description, userId, bookId);
+        const updatedBook = db
+            .prepare("SELECT * FROM Book WHERE id = ?")
+            .get(bookId);
+        res.status(200).json(updatedBook);
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Somethin went wrong" })
+    }
+})
+app.post("/api/books/:id/reviews", auth, (req, res) => {
+    try {
+        const { rating, comment } = req.body
+        const userid = req.user.id;
+        const bookid = req.params.id;
+        const query = db.prepare(`INSERT INTO Review (bookId,userId,rating,comment) VALUES(?,?,?,?)`).run(bookid, userid, rating, comment);
+        const newReviw = db.prepare(`SELECT * FROM Review WHERE bookid = ?`).get(bookid);
+        res.status(201).json(newReviw);
+    } catch (error) {
+        console.error(error)
+            res.status(500).json({ error: "something went wrong"})
         
+    }
+})
+app.get("/api/books/:id/reviews", (req, res) => {
+    try {
+        const book = db.prepare("SELECT * FROM Review WHERE bookId = ?").get(req.params.id)
+        res.status(200).json(book)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Somethin went wrong" })
+    }
+})
+app.delete("/api/reviews/:id", auth, (req, res) => {
+    try {
+        const reviewId = req.params.id;
+        const deleteook = db.prepare("DELETE FROM Review WHERE id = ?").run(reviewId);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Somethin went wrong" })
     }
 })
 app.listen(3001)
